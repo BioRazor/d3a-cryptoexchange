c753c03390dd679efb904abbe447b7afabf6b989
D3APC <BioRazor.a@gmail.com>
2019-12-22 21:35:11 -0400

    Proyecto Terminado

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-xl text.center">Platzi Exchane v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener la cotizaciones de las cryptomonedas más importantes
+      a traves de <b>Vue.js</b>. <br />Este proteto es utilizado en el curso de
+      Vue.js basico de Platzi.
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la pagina de inicio
+    </router-link>
+  </div>
+</template>
